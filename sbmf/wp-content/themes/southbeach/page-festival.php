c6644b84d749c1e7a175cbc44b267efa7e331592
juanamarcos <juana.marcos.538@my.csun.edu>
2015-03-31 16:08:32 -0700

    added sponsor logos

--- sbmf/wp-content/themes/southbeach/page-festival.php
+++ sbmf/wp-content/themes/southbeach/page-festival.php
@@ -15,14 +15,22 @@ $args = array (
 
 
 if ($festival->have_posts()) : while ($festival->have_posts()) : $festival->the_post(); ?>
-<div class="col-xs-4">
-	<div class="well well-options">
 
-			<a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a>
- 	 		<?php the_content(); ?>
+
+		<div class="col-xs-4">
+			<div class="row">
+				<div class="well">
+
+					<a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a>
+		 	 		<?php the_content(); ?>
+
+					
+				</div>
+			</div>
 
 	</div>
-</div>
+	
+
  	
 
   <?php endwhile; else: ?>
@@ -36,10 +44,14 @@ if ($festival->have_posts()) : while ($festival->have_posts()) : $festival->the_
 
 
 <!-- dates image-->
-<div class="col-xs-12 date-img">
 
- <img src="<?php bloginfo('template_directory'); ?>/images/ticket-img.png" alt="">
+<div class="col-xs-12 festival-img">
+	<div class="well well-fest">
+
+ <img src="<?php bloginfo('template_directory'); ?>/images/fest-img.png" alt="">
 </div>
+</div>
+
 
 
 <!-- end of dates image-->
