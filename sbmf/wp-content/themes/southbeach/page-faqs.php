24847fb9df4339f5b9649e9f4b8afa0a1c1abb9c
Maria Rosales <maraiajrosales8@gmail.com>
2015-03-31 16:10:04 -0700

    worked on my stuff

--- sbmf/wp-content/themes/southbeach/page-faqs.php
+++ sbmf/wp-content/themes/southbeach/page-faqs.php
@@ -7,6 +7,18 @@ Template Name: Faq Page
 
 
 <div class="row">
+	<div class="container">
+		<div class="col-xs-12">
+			<h1>General Questions and Rules</h1>
+			<ul>
+				<li>Festival starts at 11am</li>
+				<li>Rain or shine!</li>
+				<li>Festival pass must be worn at all times</li>
+				<li>Everyone is subject to search upon entry</li>
+				<li>Line up/set times are subjec to change</li>
+			</ul>
+		</div>
+	</div>
 	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 
 
