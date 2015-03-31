d41292fbcb5e71ca6973e589633c8b395f7c9c42
Maria Rosales <maraiajrosales8@gmail.com>
2015-03-30 19:58:54 -0700

    img

--- sbmf/wp-content/themes/southbeach/index.php
+++ sbmf/wp-content/themes/southbeach/index.php
@@ -1,20 +1,104 @@
 <?php get_header(); 
-
 /*
 Template Name: Home Page
 */
-
-
 ?>
 
-<?php if(have_posts()): while (have_posts()) : the_post(); ?>
+<div class="row">
+	<div class="container">
+		<div class="col-xs-12">
+			<div id="carousel" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+				    <li data-target="#carousel" data-slide-to="1"></li>
+				    <li data-target="#carousel" data-slide-to="2"></li>
+				    <li data-target="#carousel" data-slide-to="3"></li>
+				    <li data-target="#carousel" data-slide-to="4"></li>
+				  </ol>
 
-      <h2>
-      	<a href="<?php the_permalink() ?>">
-      		<?php the_title(); ?>
-      	</a>
-      </h2>
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+				    <div class="item active">
+				      <img src="images/slide1.jpg" alt="miami">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+				    <div class="item">
+				      <img src="images/slide2.jpg" alt="miami">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+				    <div class="item">
+				      <img src="images/slide3.jpg" alt="miami">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				  	</div>
+				    <div class="item">
+				      	<img src="images/slide4.jpg" alt="miami">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+				    <div class="item">
+				      	<img src="images/slide5.jpg" alt="miami">
+				      <div class="carousel-caption">
+				        ...
+				      </div>
+				    </div>
+ 
 
-<?php endwhile; endif; ?>
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="conatiner">
+		<div class="col-xs-6 row1">
+			<div class="col-xs-6">
+				<img src="<?php bloginfo('template_directory'); ?>/images/img1.jpg">
+			</div>
+
+			<div class="col-xs-6">
+				<img src="<?php bloginfo('template_directory'); ?>/images/img1.jpg">
+			</div>
+			
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="container">
+		<div class="col-xs-4">
+			<div class="col-xs-6">
+				<img src="<?php bloginfo('template_directory'); ?>/images/img3.jpg">
+			</div>
+
+			<div class="col-xs-6">
+				<img src="<?php bloginfo('template_directory'); ?>/images/img4.jpg">
+			</div>
+
+			<div class="col-xs-6">
+				<img src="<?php bloginfo('template_directory'); ?>/images/img5.jpg">
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
 
 <?php get_footer(); ?>
--- sbmf/wp-content/themes/southbeach/page-faqs.php
+++ sbmf/wp-content/themes/southbeach/page-faqs.php
@@ -1,18 +1,52 @@
-<?php get_header();
+<?php get_header(); 
 /*
-Template Name: About Page
+Template Name: FAQ Page
 */
 ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-  	<h1><?php the_title(); ?></h1>
- 	<?php the_content(); ?>
+<div class="row">
+	<div class="container">
+		<div class="faq">
+			<h1>General Rules and Policies</h1>
+		</div>
+		
+		<div class="rules">
+			<div class="col-xs-6">
+				<ul>
+					<li>Venue gates open at 11 am each day</li>
+					<li>Day parking is free</li>
+					<li>Everyone is subject to search upon entry</li>
+					<li>Festival passes must be worn at all times</li>
+					<li>Line up is subject to change</li>
+				</ul>
+			</div>
+		</div>
+
+			<div class="col-xs-6">
+				<ul>
+					<li>Venue gates open at 11 am each day</li>
+					<li>Day parking is free</li>
+					<li>Everyone is subject to search upon entry</li>
+					<li>Festival passes must be worn at all times</li>
+					<li>Line up is subject to change</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
 
 
-  <?php endwhile; else: ?>
-    <p>Sorry, no pages matched your criteria.</p>
-<?php endif; ?>
-
-
-<?php get_footer(); ?>
+<div class="row">
+	<div class="container">
+		<div class="faq">
+			<h1>General Questions</h1>
+		</div>
+		
+		<div class="questions">
+			<div class="col-xs-6">
+				
+			</div>
+		</div>
+	</div>
+</div>
