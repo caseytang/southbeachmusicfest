46145dc6dd01a2d380161252a9a808cd050c1ff6
juanamarcos <juana.marcos.538@my.csun.edu>
2015-03-30 21:22:44 -0700

    working on festival page

--- sbmf/wp-content/themes/southbeach/page-festival.php
+++ sbmf/wp-content/themes/southbeach/page-festival.php
@@ -4,16 +4,33 @@ Template Name: festival Page
 */
 ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php 
 
-  	<h1><?php the_title(); ?></h1>
- 	<?php the_content(); ?>
 
+$args = array (
+	'post_type' => 'festival'
+				);
+
+ $festival = new WP_Query($args);
+
+
+if ($festival->have_posts()) : while ($festival->have_posts()) : $festival->the_post(); ?>
+<div class="col-xs-4">
+	<div class="well well-options">
+
+			<a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a>
+ 	 		<?php the_content(); ?>
+
+	</div>
+</div>
+ 	
 
   <?php endwhile; else: ?>
     <p>Sorry, no pages matched your criteria.</p>
-<?php endif; ?>
+<?php endif; 
 
+
+?>
 
 
 
@@ -26,138 +43,4 @@ Template Name: festival Page
 
 
 <!-- end of dates image-->
-
-
-
-
-
-
-
-<!-- buy your tickets-->
-<div class="row">
-	<div class="well well-buyhere col-xs-12">
-
-		<h1 class="buy">BUY YOUR TICKETS <a href="http://www.ticketmaster.com/section" target="_blank"><span> HERE</span></a></h1>
-		
-
-
-	</div>
-		
-
-</div>
-
-<!-- end of buy your tickets-->
-	
-
-<!-- ticket option 1-->
-
-<div class="col-xs-12">
-
-<div class="col-xs-6">
-	<div class="row">
-		<a href="http://www.ticketmaster.com/section" target="_blank"> 
-		
-			<img src="<?php bloginfo('template_directory'); ?>/images/ticket.png" class="ticket-img" alt=""></a>
-	</div>
-</div>
-
-
-<div class="col-xs-5">
-	<div class="row ticket-text">
-		
-			<p>t is a long established fact that a reader 
-				will be distracted by the readable content 
-				of a page when looking at its layout. The 
-				point of using Lorem Ipsum is that it has a 
-				more-or-less normal distribution of letters, 
-				as opposed to using 'Content here, content here', 
-				making it look like readable English. Many desktop p
-				ublishing packages and web page editors now use Lorem 
-				Ipsum as their default model text, and a search for '
-				lorem ipsum' will uncover many web sites still in the
-				ir infancy. Various versions have evolved over the years, 
-				sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-	</div>
-</div>
-</div>
-
-<!-- end of ticket option 1-->
-
-
-
-<!-- ticket option 2-->
-
-<div class="col-xs-12">
-
-<div class="col-xs-6">
-	<div class="row">
-		<a href="http://www.ticketmaster.com/section" target="_blank"> 
-		
-			<img src="<?php bloginfo('template_directory'); ?>/images/ticket-2.png" class="ticket-img" alt=""></a>
-	</div>
-</div>
-
-
-<div class="col-xs-5">
-	<div class="row ticket-text">
-		
-			<p>t is a long established fact that a reader 
-				will be distracted by the readable content 
-				of a page when looking at its layout. The 
-				point of using Lorem Ipsum is that it has a 
-				more-or-less normal distribution of letters, 
-				as opposed to using 'Content here, content here', 
-				making it look like readable English. Many desktop p
-				ublishing packages and web page editors now use Lorem 
-				Ipsum as their default model text, and a search for '
-				lorem ipsum' will uncover many web sites still in the
-				ir infancy. Various versions have evolved over the years, 
-				sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-	</div>
-</div>
-</div>
-
-<!-- end of ticket option 2-->
-
-<!-- ticket option 3-->
-
-<div class="col-xs-12">
-
-<div class="col-xs-6">
-	<div class="row">
-		<a href="http://www.ticketmaster.com/section" target="_blank"> 
-		
-			<img src="<?php bloginfo('template_directory'); ?>/images/ticket-3.png" class="ticket-img" alt=""></a>
-	</div>
-</div>
-
-
-<div class="col-xs-5">
-	<div class="row ticket-text">
-		
-			<p>t is a long established fact that a reader 
-				will be distracted by the readable content 
-				of a page when looking at its layout. The 
-				point of using Lorem Ipsum is that it has a 
-				more-or-less normal distribution of letters, 
-				as opposed to using 'Content here, content here', 
-				making it look like readable English. Many desktop p
-				ublishing packages and web page editors now use Lorem 
-				Ipsum as their default model text, and a search for '
-				lorem ipsum' will uncover many web sites still in the
-				ir infancy. Various versions have evolved over the years, 
-				sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-	</div>
-</div>
-</div>
-<!-- end of ticket option 3-->
-
-
-
-
-
-
-
-
-
 <?php get_footer(); ?>
