6d2d7ae339a3f6bee3c843ed1cf15d86cb437f2d
caseytang <caseytang@live.com>
2015-03-30 17:30:14 -0700

    assembled basic footer structure with social media links

--- sbmf/wp-content/themes/southbeach/footer.php
+++ sbmf/wp-content/themes/southbeach/footer.php
@@ -1,7 +1,15 @@
 	<div id="footer">
 		<?php wp_footer(); ?>
+		  <ul>
+      	<li><a href="#"><img src="<?php bloginfo ('template_directory'); ?>/images/facebook.png" alt=""> </a></li>
+      	<li><a href="#"><img src="<?php bloginfo ('template_directory'); ?>/images/twitter.png" alt=""> </a></li>
+      	<li><a href="#"><img src="<?php bloginfo ('template_directory'); ?>/images/instagram.png" alt=""> </a></li>
+      	<li><a href="#"><img src="<?php bloginfo ('template_directory'); ?>/images/soundcloud.png" alt=""> </a></li>
+      	<li><a href="#"><img src="<?php bloginfo ('template_directory'); ?>/images/tumblr.png" alt=""> </a></li>
+      </ul>
 	</div>
-      
+    
+
 </div><!-- x content -->
 </body>
 </html>
