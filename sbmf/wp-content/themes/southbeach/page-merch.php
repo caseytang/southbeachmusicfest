ae35e65d2d82746a31b968b65cab82b30047b982
Maria Rosales <maraiajrosales8@gmail.com>
2015-03-31 17:36:27 -0700

    fixed merch page

--- sbmf/wp-content/themes/southbeach/page-merch.php
+++ sbmf/wp-content/themes/southbeach/page-merch.php
@@ -11,44 +11,44 @@ Template Name: Merch Page
 <div class="row">
   <div class="container">
     <div class="col-xs-4">
-      <div class="well">
+      <div class="well-1">
         <img src="<?php bloginfo('template_directory'); ?>/images/m1.jpg">
-        <h4>Best plan for you best pal</h4>
+        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">Purchase</a>
       </div>
     </div>
 
     <div class="col-xs-4">
-      <div class="well">
+      <div class="well-1">
         <img src="<?php bloginfo('template_directory'); ?>/images/m2.jpg">
-        <h4>Groupon</h4>
+        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">Purchase</a>
       </div>
     </div>
 
     <div class="col-xs-4">
-      <div class="well">
+      <div class="well-1">
         <img src="<?php bloginfo('template_directory'); ?>/images/m3.jpg">
-        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">View More</a>
+        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">Purchase</a>
       </div>
     </div>
 
     <div class="col-xs-4">
-      <div class="well">
+      <div class="well-1">
         <img src="<?php bloginfo('template_directory'); ?>/images/m4.jpg">
-        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">View More</a>
+        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">Purchase</a>
       </div>
     </div>
     
     <div class="col-xs-4">
-      <div class="well">
+      <div class="well-1">
         <img src="<?php bloginfo('template_directory'); ?>/images/m5.jpg">
-        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">View More</a>
+        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">Purchase</a>
       </div>
     </div>
     
     <div class="col-xs-4">
-      <div class="well">
+      <div class="well-1">
         <img src="<?php bloginfo('template_directory'); ?>/images/m6.jpg">
-        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">View More</a>
+        <a href="#" class="btn btn-primary btn-block" data-toggle="modal" data-target="#myModal<?php echo $i;?>">Purchase</a>
       </div>
     </div>
 
